76b2a7f077d72eb2e21dc406fd4a6dd15741a93d
Toorq91 <157486467+Toorq91@users.noreply.github.com>
2024-06-05 14:34:57 +0200

    Initial commit

---
+++ Planet.cs
@@ -0,0 +1,22 @@
+﻿namespace MySolarSystem
+{
+    public class Planet
+    {
+        internal static object _Name;
+
+        public string Name { get; }
+        public int Radius { get; set; }
+        public double Volume => CalculateVolume();
+
+        public Planet(string name, int radius)
+        {
+            Name = name;
+            Radius = radius;
+        }
+
+        private double CalculateVolume()
+        {
+            return 4.0 / 3.0 * Math.PI * Math.Pow(Radius, 3);
+        }
+    }
+}
